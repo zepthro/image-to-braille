f28be93e680f77b08c81e2f3df66cd0e83331e6a
Zepthro <96galaxy96@gmail.com>
2021-03-09 14:54:32 -0500

    Added a method for getting url images, and files

--- braille-stuff.py
+++ braille-stuff.py
@@ -1,9 +1,10 @@
-# constants, but I get they are not caps...
-asciiYDots = 4
-asciiXDots = 2
 from PIL import Image
 import argparse
 import math
+
+ASCIIYDOTS = 4
+ASCIIXDOTS = 2
+THRESHOLD = int(150)
 
 # main source code I am replicating. Find here:
 # https://lachlanarthur.github.io/Braille-ASCII-Art/dist/index.js
@@ -20,7 +21,6 @@ def to_matrix(the_data, n):
 # https://stackoverflow.com/questions/31572425/list-all-rgba-values-of-an-image-with-pil
 
 def image_data_to_braille(rgb_array):
-    threshold = int(150)
     # The original author used subarrays in this function.
     # I am taking a slightly different method than the original author, because the rgb_array is grayscale. 
     # Hence, why I do not need to get another subarray
@@ -33,7 +33,7 @@ def image_data_to_braille(rgb_array):
     # or simply not(...)
     # or the bitwise not operator on the result
     for i in range(len(dots)):
-        dots[i] = '1' if dots[i] >= threshold else '0'
+        dots[i] = '1' if dots[i] >= THRESHOLD else '0'
     # now we do some more vodoo magic
     # actually quite clever technique of binary representation of braille
     # again, see https://en.wikipedia.org/wiki/Braille_Patterns
@@ -49,10 +49,10 @@ def parse_image(image, asciiWidth):
     
     # new image height. Want to proportionately make the image fit the screen
     # not sure the exact conversion calculation here.
-    asciiHeight = math.ceil( asciiWidth * asciiXDots * ( height / width ) / asciiYDots )
+    asciiHeight = math.ceil( asciiWidth * ASCIIXDOTS * ( height / width ) / ASCIIYDOTS )
     # want equal number of asciiDots. i.e. 4x2 array
-    width = asciiWidth * asciiXDots
-    height = asciiHeight * asciiYDots
+    width = asciiWidth * ASCIIXDOTS
+    height = asciiHeight * ASCIIYDOTS
     rgb_pixels = rgb_pixels.resize((width,height)).getdata()
     # conversion of a 1d array to 2d array
     two_d_array = to_matrix(list(rgb_pixels), width)
@@ -66,45 +66,42 @@ def parse_image(image, asciiWidth):
     # . .
     # . .
     # . .
-    temp  = np.array(two_d_array)
-    for y in range(0,height, asciiYDots):
+    for y in range(0,height, ASCIIYDOTS):
         line_of_braille = ''
-        for x in range(0,width,asciiXDots):
+        for x in range(0,width,ASCIIXDOTS):
             # want to get that subsection now. starting in corner (x,y) to (width,height).
-            # in our case we want asciiXDots width, and asciiYDots height
+            # in our case we want ASCIIXDOTS width, and ASCIIYDOTS height
             # referencing: https://stackoverflow.com/questions/38049214/how-to-obtain-a-subarray-in-python-3
             # print ([i[3:6] for i in a[0:2]])
             # I got the array indices by trial and error with a debugger. I am sure there is a more
             # theoretical approach, but I recommend trial and error.
             # we want to get a subset of the image. We can easily use a package like numpy, but
             # relying on numpy makes the project dependencies huge.
-            line_of_braille += image_data_to_braille([sub[x:x+asciiXDots] for sub in two_d_array[y:y+asciiYDots]])
+            line_of_braille += image_data_to_braille([sub[x:x+ASCIIXDOTS] for sub in two_d_array[y:y+ASCIIYDOTS]])
 
         finished_image.append(line_of_braille)
     return finished_image
 
 if __name__ == '__main__':
-    # https://github.com/FranciscoMoretti/asciify-color/blob/master/asciify.py
-    # eventually I will add this feature. For now, I use direct input from command line.
-    #     import urllib.request
-    # try:
-    #     if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
-    #         urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
-    #         path = "asciify.jpg"
-    #     else:
-    #         path = sys.argv[1]
-    # urllib.request.urlretrieve(image,'image.jpg')
-
-    # https://github.com/itszn/Image-To-Braille/blob/master/braille.py
+    import requests
+    # Some of my code references: https://github.com/itszn/Image-To-Braille/blob/master/braille.py
+    # Feel free to give it a read
     try:
         ap = argparse.ArgumentParser()
         ap.add_argument('file', help='The image file to render')
-        
+        ap.add_argument('-t', required=False, help='How dark you want the pixels before recognizing. Default is 150.',
+        default=150, type=int, choices=range(0,256), metavar='[0-255]')
+
         args = ap.parse_args()
         filename = args.file
+        THRESHOLD = args.t
         # intentionally not converting to a 2d array here. If someone, or myself, wants to
         # add a 1d array functionality, great! I am sticking with 2d arrays right now. 
-        with Image.open(filename, 'r') as img:
+        # check if we need to open a file, or a url
+        # https://github.com/FranciscoMoretti/asciify-color/blob/master/asciify.py
+        # https://stackoverflow.com/questions/7391945/how-do-i-read-image-data-from-a-url-in-python
+
+        with Image.open(filename, 'r') if not(filename.startswith('http://') or filename.startswith('https://')) else Image.open(requests.get(filename, stream=True).raw) as img:
             # default asciiWidth I found online. Aparently over 500 gets laggy
             new_img = parse_image(img, 100)
             print (*new_img, sep='\n')
